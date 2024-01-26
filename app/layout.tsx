f2a03cae365214fd94673cf88f1e7284facf730e
Ikem-coded-it <105087922+Ikem-coded-it@users.noreply.github.com>
2024-01-26 13:26:07 +0100

    finished tutorial

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,19 @@
 import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
+import { Metadata } from 'next';
+ 
+export const metadata: Metadata = {
+  title: {
+    template: '%s | Acme Dashboard',
+    default: 'Acme Dashboard',
+  },
+  description: 'The official Next.js Learn Dashboard built with App Router.',
+  metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
+  keywords: ['Dashboard', 'Invoice', 'Business'],
+  authors: [{ name: 'Ikem', url: 'ikem.com' }],
+  creator: 'Onubogu Ikemefuna',
+  publisher: 'Onubogu Ikemefuna',
+};
 
 export default function RootLayout({
   children,
--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -3,6 +3,8 @@ import { z } from 'zod'; // validation library
 import { sql } from '@vercel/postgres';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
+import { signIn } from '@/auth';
+import { AuthError } from 'next-auth';
 
 const FormSchema = z.object({ // type validation with zod
   id: z.string(),
@@ -100,12 +102,30 @@ export async function updateInvoice(
 };
 
 export async function deleteInvoice(id: string) {
-  throw new Error('Failed to Delete Invoice');
   try {
     await sql`DELETE FROM invoices WHERE id = ${id}`;
     revalidatePath('/dashboard/invoices');
     return { message: 'Deleted Invoice.' };
   } catch (error) {
     return { message: 'Database Error: Failed to Delete Invoice.' };
+  }
+}
+
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData,
+) {
+  try {
+    await signIn('credentials', formData);
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case 'CredentialsSignin':
+          return 'Invalid credentials.';
+        default:
+          return 'Something went wrong.';
+      }
+    }
+    throw error;
   }
 }
